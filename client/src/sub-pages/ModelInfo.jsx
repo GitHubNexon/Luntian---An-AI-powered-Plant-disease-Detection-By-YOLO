d5d340b8cc1e495ec27fdbc2509c33070653b690
Arielle <arielleperez146@gmail.com>
2025-04-20 19:27:47 +0800

    addded the Model Evaluation Metrics

--- client/src/sub-pages/ModelInfo.jsx
+++ client/src/sub-pages/ModelInfo.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import Hero from "../components/ModelCards/Hero"
+import Welcome from "../components/ModelCards/Welcome";
+import ModelEvaluation from "../components/ModelCards/ModelEvaluation";
+import TrainingImageGallery from "../components/ModelCards/TrainingImages";
+import Feature from "../components/ModelCards/Feature";
+import Team from "../components/ModelCards/Team";
 
 const ModelInfo = () => {
   return (
     <div className="flex item-center justify-center m-2 -200 p-4 rounded text-center shadow-2xl cursor-pointer overflow-hidden relative transition-all duration-500 hover:translate-y-2  flex-col items-center  gap-2 before:absolute before:w-full hover:before:top-0 before:duration-500 before:-top-1 before:h-1 before:bg-green-400">
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab assumenda enim, sapiente in asperiores laboriosam odit dignissimos voluptatum, sunt facere incidunt harum temporibus perspiciatis, quasi cumque facilis. Architecto libero facere dolores, alias ea quaerat earum. Quis nisi itaque aliquid voluptas optio, enim incidunt porro repudiandae veritatis minima! Voluptates accusamus illo quas architecto incidunt dignissimos ratione, excepturi, unde molestias odio quos commodi rerum laborum sed eos adipisci dolorum! Cumque harum illum ut optio vel nam, doloremque aperiam excepturi tempore, eos mollitia illo voluptas. Ducimus saepe est odio nostrum corporis quod laborum libero quasi obcaecati non exercitationem fugit numquam eum nulla laudantium facere asperiores aliquid sapiente quisquam, dolore voluptates possimus molestias! Harum reprehenderit numquam earum officiis necessitatibus amet provident voluptas omnis quasi unde porro dolore perspiciatis, deserunt praesentium distinctio aliquid temporibus ex! Ullam quis, deserunt blanditiis, iusto adipisci rerum reprehenderit velit animi dolor similique id? Tenetur harum iste facere ipsa pariatur, commodi possimus necessitatibus sapiente quo cumque consequuntur distinctio odit, velit numquam eum ab reprehenderit doloremque libero praesentium enim quia saepe? Quisquam in ipsum impedit eaque excepturi eius aperiam esse tempora et. Sequi itaque saepe mollitia, officiis explicabo minus ut pariatur autem nobis aspernatur et earum nesciunt non! Fuga quidem distinctio illo.
+ 
+      <Hero />
+      <Welcome />   
+      <Feature />
+      <ModelEvaluation />
+      <TrainingImageGallery />
+      <Team />
     </div>
   );
 };
